079fb77a7c92bcc652a4c421781eab74a7f884ff
dis <dissh33@yandex.ru>
2022-01-20 13:48:27 +0300

    init ItemManagement controllers

--- ItemManagementService/Program.cs
+++ ItemManagementService/Program.cs
@@ -1,15 +1,23 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+#region Add services
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+#region Configure pipeline
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +29,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+#endregion
+
 
 app.Run();
