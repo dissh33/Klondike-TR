e712e5269e4886f7ae266ff39adbf6ef186129fc
dis <dissh33@yandex.ru>
2022-01-23 20:48:35 +0300

    Add first validation

---
+++ ItemManagementService.Api/Validators/IconAddValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ItemManagementService.Api.Commands;
+
+namespace ItemManagementService.Api.Validators;
+
+public class IconAddValidator : AbstractValidator<IconAddCommand>
+{
+    public IconAddValidator()
+    {
+        RuleFor(icon => icon.Title).MaximumLength(250);
+        RuleFor(icon => icon.FileBinary).NotEmpty();
+    }
+}
---
+++ ItemManagementService.Api/Validators/IconUpdateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ItemManagementService.Api.Commands;
+
+namespace ItemManagementService.Api.Validators;
+
+public class IconUpdateValidator : AbstractValidator<IconUpdateCommand>
+{
+    public IconUpdateValidator()
+    {
+        RuleFor(icon => icon.Id).NotEmpty();
+        RuleFor(icon => icon.Title).MaximumLength(250);
+    }
+}
