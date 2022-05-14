2ad12e10a5d20add4cef5fe44dd1cba98601c45a
dis <dissh33@yandex.ru>
2022-05-14 18:27:43 +0300

    add items test

---
+++ Items.Tests/Application/MappingTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+using AutoMapper;
+using ItemManagementService.Api.Dtos;
+using ItemManagementService.Application.Mapping;
+using ItemManagementService.Domain.Entities;
+using Xunit;
+
+namespace ItemManagementService.Tests.Application;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(config =>
+            config.AddProfile<IconProfile>());
+
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Fact]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Theory]
+    [InlineData(typeof(Icon), typeof(IconDto))]
+    [InlineData(typeof(Icon), typeof(IconFileDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        // Type without parameterless constructor
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
---
+++ Items.Tests/Domain/CollectionItemTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentAssertions;
+using ItemManagementService.Domain.Entities;
+using Xunit;
+
+namespace ItemManagementService.Tests.Domain;
+
+public class CollectionItemTests
+{
+    [Fact]
+    public void CollectionItem_ShouldConstruct_WithVariousParameters()
+    {
+        var id = Guid.NewGuid();
+        var collectionItem1 = new CollectionItem("N1", Guid.Empty, Guid.Empty, Guid.NewGuid());
+        var collectionItem2 = new CollectionItem("N2", Guid.Empty, Guid.Empty, id: id);
+        var collectionItem3 = new CollectionItem("N3", Guid.Empty, Guid.Empty, externalId: "newID");
+        var collectionItem4 = new CollectionItem();
+
+        collectionItem1.Should().BeOfType(typeof(CollectionItem));
+        collectionItem2.Should().BeOfType(typeof(CollectionItem));
+        collectionItem3.Should().BeOfType(typeof(CollectionItem));
+        collectionItem4.Should().BeOfType(typeof(CollectionItem));
+    }
+
+    [Fact]
+    public void CollectionItem_ShouldConstruct_EveryTimeWithNewGuid_WhenIdNotSpecified()
+    {
+        var item1 = new CollectionItem("N", Guid.Empty, Guid.Empty);
+        var item2 = new CollectionItem("N", Guid.Empty, Guid.Empty);
+
+        item1.Id.Should().NotBeEmpty();
+        item2.Id.Should().NotBeEmpty();
+
+        item1.Id.Should().NotBe(item2.Id);
+    }
+
+    [Fact]
+    public void CollectionItem_ShouldConstruct_WithSpecifiedId_WhenIdPassedThroughConstructor()
+    {
+        var id = Guid.NewGuid();
+        var collectionItem = new CollectionItem("N", Guid.Empty, Guid.Empty, id: id);
+
+        collectionItem.Id.Should().NotBeEmpty();
+        collectionItem.Id.Should().Be(id);
+    }
+}
