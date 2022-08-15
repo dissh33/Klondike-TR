b48427b98f78b93b684f5e2d469cb91cb830f2a6
dis <dissh33@yandex.ru>
2022-08-15 15:48:02 +0300

    CollectionItemGetFullByCollection

---
+++ src/Items/Items.Api/Queries/CollectionItem/CollectionItemGetFullByCollectionQuery.cs
@@ -0,0 +1,9 @@
+﻿using Items.Api.Dtos;
+using MediatR;
+
+namespace Items.Api.Queries.CollectionItem;
+
+public class CollectionItemGetFullByCollectionQuery : IRequest<IEnumerable<CollectionItemFullDto>>
+{
+    public Guid CollectionId { get; set; }
+}
---
+++ src/Items/Items.Application/Queries/CollectionItemHandlers/CollectionItemGetFullByCollectionHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Items.Api.Dtos;
+using Items.Api.Queries.CollectionItem;
+using Items.Application.Contracts;
+using MediatR;
+
+namespace Items.Application.Queries.CollectionItemHandlers;
+
+public class CollectionItemGetFullByCollectionHandler : IRequestHandler<CollectionItemGetFullByCollectionQuery, IEnumerable<CollectionItemFullDto>>
+{
+    private readonly IUnitOfWork _uow;
+    private readonly IMapper _mapper;
+
+    public CollectionItemGetFullByCollectionHandler(IUnitOfWork uow, IMapper mapper)
+    {
+        _uow = uow;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<CollectionItemFullDto>> Handle(CollectionItemGetFullByCollectionQuery request, CancellationToken ct)
+    {
+        var collectionItems = (await _uow.CollectionItemRepository.GetByCollection(request.CollectionId, ct)).ToList();
+
+        var iconsIds = collectionItems.Select(item => item.IconId);
+        var icons = (await _uow.IconRepository.GetRange(iconsIds, ct)).ToList();
+
+        foreach (var collectionItem in collectionItems)
+        {
+            var currentIcon = icons.First(icon => icon.Id == collectionItem.Id);
+
+            collectionItem.AddIcon(
+                currentIcon.Title,
+                currentIcon.FileBinary,
+                currentIcon.FileName,
+                currentIcon.Id);
+        }
+
+        return collectionItems.Select(collectionItem => _mapper.Map<CollectionItemFullDto>(collectionItem));
+    }
+}
