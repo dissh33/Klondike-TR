1932c0d76083b0961f45d1d89626d93b958bca7c
dis <dissh33@yandex.ru>
2022-08-21 19:59:31 +0300

    css for ItemsGrid

--- src/UI/KlondikeTR/Helpers/ImgHelper.cs
+++ src/UI/KlondikeTR/Helpers/ImgHelper.cs
@@ -4,7 +4,14 @@
     {
         public static string GetSrc(byte[]? data)
         {
-            var stringData = Convert.ToBase64String(data);
+            var stringData = Convert.ToBase64String(data ?? Array.Empty<byte>());
+
+            return $"data:image;base64,{stringData}";
+        }
+
+        public static string ToImageSrc(this byte[]? data)
+        {
+            var stringData = Convert.ToBase64String(data ?? Array.Empty<byte>());
 
             return $"data:image;base64,{stringData}";
         }
