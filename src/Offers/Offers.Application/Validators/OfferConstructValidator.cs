d2ac5131a1784b30ee17a4a46f3d9d7d3171dd59
dis <dissh33@yandex.ru>
2023-01-17 21:12:24 +0300

    Add mapping tests for  Offer-project

--- src/Offers/Offers.Application/Validators/OfferConstructValidator.cs
+++ src/Offers/Offers.Application/Validators/OfferConstructValidator.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Offers.Api.Commands;
 
 namespace Offers.Application.Validators;
-internal class OfferConstructValidator
+
+public class OfferConstructValidator : AbstractValidator<OfferConstructCommand>
 {
+    public OfferConstructValidator()
+    {
+        RuleFor(command => command.Title).Length(3, 255);
+        RuleFor(command => command.Status).InclusiveBetween(0, 5);
+        RuleFor(command => command.Type).InclusiveBetween(0, 1);
+
+
+        RuleForEach(command => command.Positions.Select(position => position.Message)).Length(0, 500);
+        RuleForEach(command => command.Positions.Select(position => position.PriceRate)).Length(4, 50);
+        RuleForEach(command => command.Positions.Select(position => position.WithTrader)).InclusiveBetween(false, true);
+        RuleForEach(command => command.Positions.Select(position => position.Type)).InclusiveBetween(0, 6);
+
+
+        RuleForEach(command => command.Positions)
+            .Must(dto => dto.OfferItems.Any());
+
+
+        RuleForEach(command => command.Positions
+            .SelectMany(position => position.OfferItems).Select(item => item.TradableItemId)).NotEmpty();
+
+        RuleForEach(command => command.Positions
+            .SelectMany(position => position.OfferItems).Select(item => item.Amount)).GreaterThanOrEqualTo(0);
+
+        RuleForEach(command => command.Positions
+            .SelectMany(position => position.OfferItems).Select(item => item.Type)).InclusiveBetween(0,1);
+    }
 }
---
+++ tests/Offers.Tests/Application/MappingTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.Serialization;
+using AutoMapper;
+using Offers.Api.Dtos;
+using Offers.Application.Mapping;
+using Offers.Domain.Entities;
+using Xunit;
+
+namespace Offers.Tests.Application;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _autoMapperConfiguration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+
+        _autoMapperConfiguration = new MapperConfiguration(config =>
+        {
+            config.AddProfile<OfferProfile>();
+            config.AddProfile<OfferPositionProfile>();
+            config.AddProfile<OfferItemProfile>();
+        });
+
+        _mapper = _autoMapperConfiguration.CreateMapper();
+    }
+
+    [Fact]
+    internal void ShouldHaveValidConfiguration()
+    {
+        _autoMapperConfiguration.AssertConfigurationIsValid();
+    }
+
+    [Theory]
+    [InlineData(typeof(Offer), typeof(OfferDto))]
+    [InlineData(typeof(OfferPosition), typeof(OfferPositionDto))]
+    [InlineData(typeof(OfferItem), typeof(OfferItemDto))]
+    internal void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private static object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        // Type without parameterless constructor
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
